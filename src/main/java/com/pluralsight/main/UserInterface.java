c827aa08b831e48884b528b9969826dd3712b3e6
keziazahiljohn <keziazahiljohn@gmail.com>
2026-05-28 12:36:29 -0400

    main menu

--- src/main/java/com/pluralsight/main/UserInterface.java
+++ src/main/java/com/pluralsight/main/UserInterface.java
@@ -5,10 +5,41 @@ import com.pluralsight.order.Order;
 import java.util.Scanner;
 
 public class UserInterface {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         Order order;
-        Scanner scanner;
+        Scanner scanner = new Scanner(System.in);
+        int choice = -1;
+        while (choice != 2) {
+            System.out.println("\nWelcome to Kezia's Pizza Shop!");
+            System.out.println("1. Start Ordering");
+            System.out.println("2. Exit");
+            System.out.print("Your choice: ");
 
-        System.out.println();
+            if (!scanner.hasNextInt()) {
+                System.out.println("Please enter 1 or 2.");
+                scanner.nextLine();                 // discard bad input
+                continue;
+            }
+            choice = scanner.nextInt();
+            scanner.nextLine();                     // clear newline
+
+            switch (choice) {
+                case 1 -> addPizza();
+                case 3 -> System.out.println("Have a saucy day :)!");
+                default -> System.out.println("Invalid choice!");
+            }
+        }
+        scanner.close();
+
+    }
+
+    public static void addPizza() {
+        System.out.println("Pick a size:");
+        System.out.println("1. 8 in.");
+        System.out.println("2. 12 in.");
+        System.out.println("3. 16 in.");
+
+        int input = Integer.parseInt(scanner.nextLine());
     }
 }
--- src/main/java/com/pluralsight/order/Item.java
+++ src/main/java/com/pluralsight/order/Item.java
@@ -1,7 +1,10 @@
 package com.pluralsight.order;
 
+import java.util.List;
+
 public abstract class Item {
     private double price;
+    private List<Item> orderItems;
 
     public Item(double price) {
         this.price = price;
